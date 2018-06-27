bfd9ba508467f9ab9fa96ddd62456ac66f6df307
Jamie Brown <jmbrowncow@gmail.com>
2018-06-27 20:26:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU TOO GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    puts "NO NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP< SONNY!"
   end
